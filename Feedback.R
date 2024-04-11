b31e27a76c9f755f7c43a7a47e9677f9913257cc
Marion Lieutaud <72838935+MarionLieutaud@users.noreply.github.com>
2024-04-11 16:12:19 +0100

    feedback

---
+++ Feedback.R
@@ -0,0 +1,29 @@
+# Feedback: CTA exercise 5 (2024)
+
+# question 1
+# -----------------------------------------------------------
+
+# question 2
+# -----------------------------------------------------------
+
+
+# question 3
+# -----------------------------------------------------------
+
+
+# overall code quality: 
+# -----------------------------------------------------------
+# you didn't knit your doc and I wasn't able to knit it myself because there were code errors early on. 
+# in future make sure you knit your doc again when you're done with the code as that is what the assessment will be based on. 
+# overall your code looks mostly fine but shows little adaptation from the exercise, and you don't comment/explain your code enough
+# (use hashtags in code lines to explain things, e.g.)
+
+table (a,b) # building a table for variables a and b
+
+# there are also code errors at various points that prevent the knitting of your doc.
+
+# overall interpretation: 
+# -----------------------------------------------------------
+# The biggest failing here is that you do not explain your analyses (e.g. why this many topics etc) and you do not interpret your analyses at alL; You just run the code but do not explain what we learn from the analysis
+# you MUST interpret outputs for the final assignment: it is a very big part of the final assessment so make sure you put a lot more focus on making sense of your analyses
+# and what they tell us about the text etc 
